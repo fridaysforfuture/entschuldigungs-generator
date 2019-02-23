1cd533bdf367b377477028008c84d1127a422239
Karl Beecken <karl.beecken@icloud.com>
2019-02-23 20:15:30 +0100

    more improvement

---
+++ web/assemble.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Informationsschreiben-Generator</title>
+  </head>
+  <body>
+
+  </body>
+</html>
+
+<?php
+
+$namefrom = $_GET['namefrom'];
+$nameto = $_GET['nameto'];
+
+
+
+?>
