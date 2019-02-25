4743eb6b2091c9db76e5fb20b8905a5e5bd09388
Karl Beecken <karl.beecken@icloud.com>
2019-02-25 16:17:19 +0100

    add pdf creator

--- web/js/script.js
+++ web/js/script.js
@@ -1,8 +1,16 @@
+function createPdf() {
+
+  var doc = new jsPDF()
+  doc.text(namefrom, 10, 10)
+  doc.save('a4.pdf')
+
+}
+
 function submit() {
 
   nameto = document.getElementById('nameto').value;
   namefrom = document.getElementById('namefrom').value;
   document.getElementById('submit-btn').innerHTML = "Gesendet!";
-  alert(nameto + namefrom);
+  createPdf();
 
 }
