8ebc63218d3eb5fc671738b191012a9acae802e6
Karl Beecken <karl.beecken@icloud.com>
2019-02-25 15:44:51 +0100

    test around with js

---
+++ web/script.js
@@ -0,0 +1,7 @@
+function submit() {
+
+  var nameto = document.getElementById('nameto').value;
+  var namefrom = document.getElementById('namefrom').value;
+  document.getElementById("demo").innerHTML = "YOU CLICKED ME!";
+
+}
