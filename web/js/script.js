05cddab04222a6f04e2ded6bae9228540d8a6d44
Karl Beecken <karl.beecken@icloud.com>
2019-02-26 18:06:05 +0100

    add a modal box

--- web/js/script.js
+++ web/js/script.js
@@ -1,3 +1,7 @@
+var nameto;
+var namefrom;
+var gender;
+
 function createPdf() {
 
   var doc = new jsPDF()
@@ -8,14 +12,19 @@ function createPdf() {
 
 function submit() {
 
-  nameto = document.getElementById('nameto').value;
-  namefrom = document.getElementById('namefrom').value;
-  gender = document.getElementById('gender').value;
+  var nameto = document.getElementById('nameto').value;
+  var namefrom = document.getElementById('namefrom').value;
+  var gender = document.getElementById('gender').value;
   document.getElementById('submit-btn').innerHTML = "Gesendet!";
   document.getElementById('submit-btn').disabled = true;
   document.getElementById('submit-btn').classList.add('btn-success');
-  document.getElementById('submit-btn').classList.remove('btn-default');
-  alert(gender + "\n" + nameto + "\n" + namefrom);
+  document.getElementById('submit-btn').classList.remove('btn-outline-success');
+  document.getElementById('submit-modal').innerHTML = "Gesendet!";
+  document.getElementById('submit-modal').classList.add('btn-success');
+  document.getElementById('submit-modal').classList.remove('btn-outline-success');
+  document.getElementById('submitModalLabel').innerHTML = "Abgeschickt!";
+  document.getElementById('modal-content').innerHTML = gender + "<br />" + nameto + "<br />" + namefrom;
+  //alert(gender + "\n" + nameto + "\n" + namefrom);
   //createPdf();
 
 }
