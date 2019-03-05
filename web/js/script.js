cbe60eb7779996f9e8a5e37d098e7907f41720c8
Karl Beecken <karl.beecken@icloud.com>
2019-03-05 21:21:59 +0100

    playing around with moment
    Signed-off-by: Karl Beecken <karl.beecken@icloud.com>

--- web/js/script.js
+++ web/js/script.js
@@ -2,25 +2,38 @@ var nameto;
 var namefrom;
 var gender;
 var salutation;
+var datefrom;
+var date2;
+var namechild;
+var childgender;
+var childsalutation;
+var timego;
+var timestart;
 
-function createPdf() {
+moment.lang("de").format('LL');
 
-  var doc = new jsPDF()
-  doc.text(namefrom, 10, 10)
-  doc.save('a4.pdf')
-
-}
-
-function submit() {
+function submit_data() {
 
   var nameto = document.getElementById('nameto').value;
   var namefrom = document.getElementById('namefrom').value;
+  var datefrom = document.getElementById('datefrom').value;
+  var timestart = document.getElementById('timestart').value;
+  var namechild = document.getElementById('namechild').value;
+  var timego = document.getElementById('timego').value;
   var gender = document.getElementById('gender').value;
   if (gender === "women") {
     var salutation = "Sehr geehrte Frau ";
   } else {
     var salutation = "Sehr geehrter Herr ";
   }
+  var childgender = document.getElementById('childgender').value;
+  if (childgender === "daughter") {
+    var childsalutation = "meine Tochter ";
+  } else {
+    var childsalutation = "meinen Sohn ";
+  }
+
+  alert(date2 = moment(datefrom, 'YYYY-MM-DD'));
   document.getElementById('submit-btn').innerHTML = "Gesendet!";
   document.getElementById('submit-btn').disabled = true;
   document.getElementById('submit-btn').classList.add('btn-success');
@@ -29,7 +42,27 @@ function submit() {
   document.getElementById('submit-modal').classList.add('btn-success');
   document.getElementById('submit-modal').classList.remove('btn-outline-success');
   document.getElementById('submitModalLabel').innerHTML = "Abgeschickt!";
-  document.getElementById('modal-content').innerHTML = salutation + nameto + "<br>" + namefrom;
-  //createPdf();
+  document.getElementById('modal-content').innerHTML = salutation + nameto + "<br>" + namefrom + "<br>" + datefrom;
+  var docDefinition = {
+    content: [
+      {
+        text: 'Beurlaubung '+  namefrom + '\n\n\n',
+        style: 'title'
+      },
+      {
+        text: salutation + nameto + ',\n\n'
+      },
+      {
+        text: 'Hiermit bitte ich darum, ' + childsalutation + namechild + ' am ' + dateformatted + ' ab ' + timego + ' Uhr zu beurlauben.\n'
+      }
+    ],
+    styles: {
+      title: {
+        fontSize: 28,
+        bold: true
+      }
+    }
+  };
 
+  pdfMake.createPdf(docDefinition).open({}, window);
 }
